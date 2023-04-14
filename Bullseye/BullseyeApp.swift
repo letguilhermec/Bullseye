683dd04c3f4ae34ab26fab4b996a5c5b280cb11c
Guilherme Correa <correacgui@gmail.com>
2023-04-14 14:53:56 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Guilherme de Carvalho Correa on 14/04/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
